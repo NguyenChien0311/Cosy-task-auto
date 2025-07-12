b021db15d1ecfd01fe8f7739f99883541225f247
chien <chien031103@gmail.com>
2025-07-13 01:57:18 +0700

    Initial commit - upload React project

--- api/task-member.js
+++ api/task-member.js
@@ -1,33 +1,30 @@
-// /api/task-member.js
 export default async function handler(req, res) {
-    console.log('req: ', req);
-    if (req.method !== "POST") {
-      return res.status(405).json({ error: "Only POST is allowed" });
-    }
-  
-    const { taskId, token } = req.body;
-  
+    // if (req.method !== "POST") {
+    //     return res.status(405).json({ error: "Method not allowed" });
+    // }
+
+    const { taskId, token, companyId } = req.body;
+
     try {
-      const response = await fetch("https://api.cosyfoto.com/", {
-        method: "POST",
-        headers: {
-          Authorization: `JWT ${token}`,
-          Companyid: '',
-          "Content-Type": "application/json",
-          Accept: "application/json",
-          Namespace: "CMS",
-        },
-        body: JSON.stringify({
-          m: "taskMember",
-          fn: "get-list-member",
-          taskId,
-        }),
-      });
-  
-      const data = await response.json();
-      return res.status(response.status).json(data);
-    } catch (error) {
-      return res.status(500).json({ error: "Proxy failed", details: error.message });
+        const apiResponse = await fetch("https://api.cosyfoto.com/", {
+            method: "POST",
+            headers: {
+                Authorization: `JWT ${token}`,
+                Companyid: companyId,
+                "Content-Type": "application/json",
+                Accept: "application/json",
+                Namespace: "CMS",
+            },
+            body: JSON.stringify({
+                m: "taskMember",
+                fn: "get-list-member",
+                taskId,
+            }),
+        });
+
+        const data = await apiResponse.json();
+        return res.status(apiResponse.status).json(data);
+    } catch (err) {
+        return res.status(500).json({ error: "Proxy API failed" });
     }
-  }
-  
+}
