fb2bd71661b9d3c73fa128d685bcc748213ac519
chien <chien031103@gmail.com>
2025-07-13 01:43:00 +0700

    Initial commit - upload React project

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const App = () => {
     setLoading(true);
   
     try {
-      const response = await fetch("/", {
+      const response = await fetch("/api/task-member", {
         method: "POST",
         headers: {
           Authorization: `JWT ${token}`,
